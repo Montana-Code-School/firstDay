fb589520585d8bb8c5bf1253bc49acf51372afc7
kramduckner <buckner.a.mark@gmail.com>
2018-02-10 15:58:11 -0700

    Create lib.js

---
+++ lib.js
@@ -0,0 +1,9 @@
+//There are tons of libraries out there. Some are shipped with javascript "out of the box" or "natively" and we can use
+//them without having to fetch someone else code to use them. 
+
+//for example, in javascript, there is the Date object. 
+
+
+// Check out the docs on Date at https://www.w3schools.com/jsref/jsref_obj_date.asp
+// Using dev docs is another skill you'll have to get accustomed to. Use the docs to console log
+// the current date as a UTC string
