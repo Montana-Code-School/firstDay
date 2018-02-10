963ad64d0cc303f3e05b0922e76c2a66c08a7c95
kramduckner <buckner.a.mark@gmail.com>
2018-02-10 15:53:26 -0700

    Create str.js

---
+++ str.js
@@ -0,0 +1,11 @@
+//a string is just a sequence of letters, numbers, and any keyboard characters wrapped in quotes. Here's an example
+//"this is a string. I think you are cool"
+
+//notice a few things - although we made a couple sentences, it's up to the coders to ensure grammatical correctedness. 
+//Also, the white space in between each word is considered a character and part of the string. 
+
+//Make a for loop and log each letter in the string above. Notice the white space and the period.
+
+
+//finally, "build" a string by adding two strings together. They can be anything you want. Just notice that we can 
+//compose strings out of other strings. 
